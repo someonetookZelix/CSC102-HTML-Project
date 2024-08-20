7a0bd02f1839c716e823ce734b2c82830aba29df
Nathan <87598494+someonetookZelix@users.noreply.github.com>
2024-08-20 14:15:12 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,23 +1,18 @@
-function palindrome() {
-    let answer = prompt("Please enter a word:", "Radar");
-    checkPalindrome(answer);
-}
-
-function checkPalindrome (str) {
-    return str == str.split("").reverse().join("");
-  }
-
 function updateText() {
     document.getElementById("heading").innerHTML = ""
     document.getElementById("meme").src = "stonks.png"
 }
 
+//Play Game Function
 function playCraps() {
+    //Roll the Dice
     var die1 = Math.floor(Math.random() * 6);
     var die2 = Math.floor(Math.random() * 6);
+    //Add rolls together
     var sum = die1 + die2;
     var result = ""
 
+    //Compare Values to determine result
     if (sum == 7 || sum == 11) {
         result = "You Lose!"
     }
@@ -28,6 +23,7 @@ function playCraps() {
         result = "You pushed!"
     }
 
+    //Set text for the rolls and the result of the game
     document.getElementById("results").innerText = die1 + ", " + die2;
     document.getElementById("output").innerText = result;
 }
