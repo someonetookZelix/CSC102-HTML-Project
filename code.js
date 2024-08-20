54bb11729d984e3352a85d0d87b658cb0487ade6
Nathan <87598494+someonetookZelix@users.noreply.github.com>
2024-08-20 15:03:21 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -27,3 +27,29 @@ function playCraps() {
     document.getElementById("results").innerText = die1 + ", " + die2;
     document.getElementById("output").innerText = result;
 }
+
+//Ask prompt when button is pressed
+function checkWord() {
+    let word = prompt("Please enter a word")
+
+    checkPalindrome(word);
+}
+
+//Check if word is a palindrome
+function checkPalindrome(str) {
+    var re = /[\W_]/g;
+    //replace symbols and capital letters
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    //reverse the word
+    var revWord = lowRegStr.split('').reverse().join('');
+
+    //check if word is a palindrome by comparison
+    if (lowRegStr == revWord) {
+        let result = prompt("That is a palindrome! Would you like to enter another word?")
+        checkPalindrome(result);
+    }
+    else {
+        let result = prompt("That is not a palindrome. Please try another word.")
+        checkPalindrome(result);
+    }
+}
